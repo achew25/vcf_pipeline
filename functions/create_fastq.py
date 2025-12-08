7a072e59befa24f1f2af3cf8d9542235c0f78ffb
achew25 <antonypme.chew@gmail.com>
2025-12-08 21:45:29 +0000

    21:45

--- functions/create_fastq.py
+++ functions/create_fastq.py
@@ -9,19 +9,20 @@ def create_fastq(file_path_and_name, mutated_fragments, quality_score_ascii):
 
     with open(file_path_and_name, 'w') as f:
         for i in range(max_read_id):
-            read_id = 1
-            f.write(f"@{read_id[i]}-->fasta_details[0]\n")
+            read_number = i + 1
+            f.write(f"@{read_number}-->{fasta_details[0]}\n")
             #Sequence
-            f.write(f"{mutated_fragments[i]}\n")
+            f.write(f"{mutated_fragments[read_number]}\n")
             #+ sign
             f.write("+\n")                
             #Quality Score
             #for base in range(len(mutated_fragments[read_id - 1])):
-            quality_score_line = quality_score_ascii * len(mutated_fragments[read_id - 1])
+            quality_score_line = quality_score_ascii * len(mutated_fragments[read_number])
             f.write(f"{quality_score_line}\n")
 
             if len(mutated_fragments[i]) != len(quality_score_ascii[i]):
-                print(f"Skipping read {read_id[i]}: Sequence and quality lengths do not match.")
+                print(f"Skipping read {read_number}: Sequence and quality lengths do not match.")
                 continue
+            
     
     return f"\nSuccessfully created FASTQ file in {file_path_and_name}"
